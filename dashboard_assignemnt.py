462455d775be7cb3a1558674d3e9e15f3343ec0d
subzero161 <58404587+subzero161@users.noreply.github.com>
2022-11-17 11:44:57 -0500

    update

--- dashboard_assignemnt.py
+++ dashboard_assignemnt.py
@@ -3,13 +3,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+STAFF_COLUMN = 'date/time'
+DATA_URL = 'https://healthdata.gov/resource/nqtp-eetp.csv'
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+
+def load_data():
+    data = pd.read_csv(DATA_URL)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data   
+
+df = load_data()
+#df = df.reset_index()
+df
+
+##check pandas pivot function
